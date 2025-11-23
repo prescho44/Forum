4df194e0a03bef9dca17f5168d42157a4fb5381a
Preslav Stankov <preslav.stankov@gmail.com>
2025-11-24 00:59:50 +0200

    update firebase

--- src/config/firebase-config.js
+++ src/config/firebase-config.js
@@ -1,19 +1,21 @@
-import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
-import { getDatabase } from 'firebase/database';
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getDatabase } from "firebase/database";
+import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDFbe_fq0T_9Ve1jEv_TbZ9xb-yXbaWrBU',
-  authDomain: 'forum-64b81.firebaseapp.com',
+  apiKey: "AIzaSyABha3JbCcc29EcxgLNbZs0EcFWXm7Mn7M",
+  authDomain: "forum-53cfe.firebaseapp.com",
   databaseURL:
-    'https://forum-64b81-default-rtdb.europe-west1.firebasedatabase.app',
-  projectId: 'forum-64b81',
-  storageBucket: 'forum-64b81.firebasestorage.app',
-  messagingSenderId: '1001058422129',
-  appId: '1:1001058422129:web:6fb09e104639d205053ad0',
-  measurementId: 'G-64WRXCQ5HG',
+    "https://forum-53cfe-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "forum-53cfe",
+  storageBucket: "forum-53cfe.firebasestorage.app",
+  messagingSenderId: "877609387828",
+  appId: "1:877609387828:web:04d5edd7344095001c8761",
+  measurementId: "G-4HG2478YYG",
 };
 
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getDatabase(app);
+export const analytics = getAnalytics(app);
